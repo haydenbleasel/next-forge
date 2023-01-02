f6de5edf832f65d5df9a1e63437cc52d597b290a
Hayden Bleasel <hello@haydenbleasel.com>
2023-01-01 23:45:31 -0500

    Add next secure headers

---
+++ src/next.config.mjs
@@ -0,0 +1,30 @@
+import { createSecureHeaders } from 'next-secure-headers';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    domains: [],
+  },
+  headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          ...createSecureHeaders(),
+          // HSTS Preload: https://hstspreload.org/
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
