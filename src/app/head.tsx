73871f7884961a69b943a7ddeb76f4130d7c612e
Hayden Bleasel <hello@haydenbleasel.com>
2023-01-01 23:32:44 -0500

    Scaffold page and head

---
+++ src/app/head.tsx
@@ -0,0 +1,5 @@
+import type { FC } from 'react';
+
+const Head: FC = () => <meta charSet="utf-8" />;
+
+export default Head;
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import type { FC } from 'react';
+
+const Home: FC = () => <p>Hello, world.</p>;
+
+export default Home;
