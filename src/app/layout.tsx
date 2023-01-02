70e21c32e556383fb774ee2516ac81f2743bae02
Hayden Bleasel <hello@haydenbleasel.com>
2023-01-01 23:30:36 -0500

    Add tsconfig to root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,10 @@ import type { FC, ReactNode } from 'react';
 
 const Layout: FC<{
   children: ReactNode;
-}> = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-};
+}> = ({ children }) => (
+  <html lang="en">
+    <body>{children}</body>
+  </html>
+);
 
 export default Layout;
