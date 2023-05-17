198abf6eefeb962c94f7276676f2785742dea3e0
Hayden Bleasel <hello@haydenbleasel.com>
2023-05-17 16:35:48 -0400

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,12 @@
+/* eslint-disable n/no-sync */
+// eslint-disable-next-line import/no-nodejs-modules
+import fs from 'node:fs';
 import type { MetadataRoute } from 'next';
+
+const appFolders = fs.readdirSync('app', { withFileTypes: true });
+const pages = appFolders
+  .filter((file) => file.isDirectory())
+  .map((folder) => folder.name);
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
@@ -6,5 +14,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: new URL(process.env.NEXT_PUBLIC_SITE_URL ?? '').href,
       lastModified: new Date(),
     },
+    ...pages.map((page) => ({
+      url: new URL(page, process.env.NEXT_PUBLIC_SITE_URL).href,
+      lastModified: new Date(),
+    })),
   ];
 }
