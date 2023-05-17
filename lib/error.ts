3b12a33bf3416748525000e6764c6365dae97574
Hayden Bleasel <hello@haydenbleasel.com>
2023-05-17 15:50:13 -0400

    Add handleError function

--- lib/error.ts
+++ lib/error.ts
@@ -1,5 +1,13 @@
+import { toast } from '@beskar-labs/gravity/toast';
+
 export const parseError = (error: unknown): string => {
   const message = error instanceof Error ? error.message : String(error);
 
   return message;
+};
+
+export const handleError = (error: unknown): void => {
+  const message = parseError(error);
+
+  toast.error(message);
 };
