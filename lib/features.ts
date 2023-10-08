5e56cdbd63ef084ecf02bffb10056ec07a379978
Hayden Bleasel <hello@haydenbleasel.com>
2023-10-08 18:47:27 -0400

    Add stack logos

--- lib/features.ts
+++ lib/features.ts
@@ -4,94 +4,110 @@ export const features = [
     description:
       'Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.',
     link: 'https://ui.shadcn.com/',
+    image: '/stack/shadcn-ui.svg',
   },
   {
     name: 'Radix UI',
     description:
       'An open source component library optimized for fast development, easy maintenance, and accessibility. Just import and go—no configuration required.',
     link: 'https://www.radix-ui.com/',
+    image: '/stack/radix-ui.svg',
   },
   {
     name: 'Tailwind CSS',
     description:
       'Rapidly build modern websites without ever leaving your HTML.',
     link: 'https://tailwindcss.com/',
+    image: '/stack/tailwind.svg',
   },
   {
     name: 'PlanetScale',
     description:
       'PlanetScale is the world’s most advanced MySQL platform, powered by open source tech.',
     link: 'https://planetscale.com/',
+    image: '/stack/planetscale.svg',
   },
   {
     name: 'Prisma',
     description:
       'Next-generation Node.js and TypeScript ORM, unlocking a new level of developer experience when working with databases thanks to its intuitive data model, automated migrations, type-safety & auto-completion.',
     link: 'https://www.prisma.io/',
+    image: '/stack/prisma.svg',
   },
   {
     name: 'eslint-config-harmony',
     description:
       'A strict, opinionated ESLint config for modern TypeScript apps, designed to work seamlessly together and enforce hyper-strict syntax rules as you type to help you write bulletproof code.',
     link: 'https://github.com/haydenbleasel/eslint-config-harmony',
+    image: '/stack/harmony.svg',
   },
   {
     name: 'Next.js 13',
     description:
       'The React Framework for the Web. Create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.',
     link: 'https://nextjs.org/',
+    image: '/stack/nextjs.svg',
   },
   {
     name: 'Vercel',
     description:
       "Vercel's frontend cloud gives developers the frameworks, workflows, and infrastructure to build a faster, more personalized Web.",
     link: 'https://vercel.com/',
+    image: '/stack/vercel.svg',
   },
   {
     name: 'react.email',
     description:
       'A collection of high-quality, unstyled components for creating beautiful emails using React and TypeScript.',
     link: 'https://react.email/',
+    image: '/stack/react-email.svg',
   },
   {
     name: 'Clerk',
     description:
       'Clerk is more than a "sign-in box." Integrate complete user management UIs and APIs, purpose-built for React, Next.js, and the Modern Web.',
     link: 'https://clerk.com/',
+    image: '/stack/clerk.svg',
   },
   {
     name: 'Resend',
     description:
       'Email for. developers. The best API to reach humans instead of spam folders. Build, test, and deliver transactional emails at scale.',
     link: 'https://resend.com/',
+    image: '/stack/resend.svg',
   },
   {
     name: 'Loops',
     description:
       'Email for modern SaaS. A better way to send marketing + transactional email',
     link: 'https://loops.so/',
+    image: '/stack/loops.svg',
   },
   {
     name: 'next-secure-headers',
     description: 'Sets secure response headers for Next.js.',
     link: 'https://www.npmjs.com/package/next-secure-headers',
+    image: '/stack/next-secure-headers.svg',
   },
   {
     name: 'BetterStack',
     description:
       'Better Stack lets you see inside any stack, debug any issue, and resolve any incident.',
     link: 'https://betterstack.com/',
+    image: '/stack/betterstack.svg',
   },
   {
     name: 'Google Fonts',
     description:
       'next-forge is setup with Inter and IBM Plex Mono, wired up through your Tailwind config as CSS variables.',
     link: 'https://fonts.google.com/',
+    image: '/stack/google-fonts.svg',
   },
   {
     name: 'Vercel AI + OpenAI',
     description:
       'The Vercel AI SDK is a library for building AI-powered streaming text and chat UIs.',
     link: 'https://www.npmjs.com/package/ai',
+    image: '/stack/openai.svg',
   },
 ];
