6a4ac7be70fe98ce845a8a616ca7e74687ffd444
Hayden Bleasel <hello@haydenbleasel.com>
2023-10-08 13:28:34 -0400

    Build chat feature to demonstrate AI

---
+++ app/components/chat.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { useChat } from 'ai/react';
+import { ChatBubbleIcon, PaperPlaneIcon } from '@radix-ui/react-icons';
+import { Button } from '@/components/ui/button';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+import { Input } from '@/components/ui/input';
+import type { FC } from 'react';
+
+export const Chat: FC = () => {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  return (
+    <div className="fixed bottom-4 right-4">
+      <Popover>
+        <PopoverTrigger>
+          <Button size="icon">
+            <ChatBubbleIcon />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent
+          className={cn(
+            'h-[50vh] w-[20rem] flex flex-col divide-y p-0 overflow-hidden',
+            'bg-neutral-100 divide-neutral-200',
+            'dark:bg-neutral-900 dark:divide-neutral-800'
+          )}
+          collisionPadding={16}
+        >
+          <div className="flex-1 p-4 overflow-y-auto text-sm flex flex-col gap-2">
+            {messages.map((message) => (
+              <div
+                key={message.id}
+                className={
+                  message.role === 'user'
+                    ? 'text-neutral-500'
+                    : 'text-neutral-900 dark:text-white'
+                }
+              >
+                {message.content}
+              </div>
+            ))}
+          </div>
+
+          <form
+            onSubmit={handleSubmit}
+            className="shrink-0 flex flex-col gap-1 bg-white"
+          >
+            <Input
+              value={input}
+              placeholder="Ask me something about next-forge"
+              onChange={handleInputChange}
+              className="rounded-none border-none shadow-none py-6 pb-16"
+            />
+            <div className="absolute bottom-0 right-0 p-4">
+              <Button type="submit" variant="outline" size="icon">
+                <PaperPlaneIcon />
+              </Button>
+            </div>
+          </form>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import { createMetadata } from '@/lib/metadata';
 import { Card } from '@/components/ui/card';
+import { features } from '@/lib/features';
+import { Chat } from './components/chat';
 import type { Metadata } from 'next';
 import type { FC } from 'react';
 
@@ -9,98 +11,6 @@ const description =
   'Robust and comprehensive boilerplate for modern Next.js web apps.';
 
 export const metadata: Metadata = createMetadata(title, description);
-
-const features = [
-  {
-    name: 'shadcn/ui',
-    description:
-      'Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.',
-    link: 'https://ui.shadcn.com/',
-  },
-  {
-    name: 'Radix UI',
-    description:
-      'An open source component library optimized for fast development, easy maintenance, and accessibility. Just import and go—no configuration required.',
-    link: 'https://www.radix-ui.com/',
-  },
-  {
-    name: 'Tailwind CSS',
-    description:
-      'Rapidly build modern websites without ever leaving your HTML.',
-    link: 'https://tailwindcss.com/',
-  },
-  {
-    name: 'PlanetScale',
-    description:
-      'PlanetScale is the world’s most advanced MySQL platform, powered by open source tech.',
-    link: 'https://planetscale.com/',
-  },
-  {
-    name: 'Prisma',
-    description:
-      'Next-generation Node.js and TypeScript ORM, unlocking a new level of developer experience when working with databases thanks to its intuitive data model, automated migrations, type-safety & auto-completion.',
-    link: 'https://www.prisma.io/',
-  },
-  {
-    name: 'eslint-config-harmony',
-    description:
-      'A strict, opinionated ESLint config for modern TypeScript apps, designed to work seamlessly together and enforce hyper-strict syntax rules as you type to help you write bulletproof code.',
-    link: 'https://github.com/haydenbleasel/eslint-config-harmony',
-  },
-  {
-    name: 'Next.js 13',
-    description:
-      'The React Framework for the Web. Create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.',
-    link: 'https://nextjs.org/',
-  },
-  {
-    name: 'Vercel',
-    description:
-      "Vercel's frontend cloud gives developers the frameworks, workflows, and infrastructure to build a faster, more personalized Web.",
-    link: 'https://vercel.com/',
-  },
-  {
-    name: 'react.email',
-    description:
-      'A collection of high-quality, unstyled components for creating beautiful emails using React and TypeScript.',
-    link: 'https://react.email/',
-  },
-  {
-    name: 'Clerk',
-    description:
-      'Clerk is more than a "sign-in box." Integrate complete user management UIs and APIs, purpose-built for React, Next.js, and the Modern Web.',
-    link: 'https://clerk.com/',
-  },
-  {
-    name: 'Resend',
-    description:
-      'Email for. developers. The best API to reach humans instead of spam folders. Build, test, and deliver transactional emails at scale.',
-    link: 'https://resend.com/',
-  },
-  {
-    name: 'Loops',
-    description:
-      'Email for modern SaaS. A better way to send marketing + transactional email',
-    link: 'https://loops.so/',
-  },
-  {
-    name: 'next-secure-headers',
-    description: 'Sets secure response headers for Next.js.',
-    link: 'https://www.npmjs.com/package/next-secure-headers',
-  },
-  {
-    name: 'BetterStack',
-    description:
-      'Better Stack lets you see inside any stack, debug any issue, and resolve any incident.',
-    link: 'https://betterstack.com/',
-  },
-  {
-    name: 'Google Fonts',
-    description:
-      'next-forge is setup with Inter and IBM Plex Mono, wired up through your Tailwind config as CSS variables.',
-    link: 'https://fonts.google.com/',
-  },
-];
 
 const Features: FC = () => (
   <section className="py-16 bg-neutral-100">
@@ -136,6 +46,7 @@ const Hero: FC = () => (
       <span className="max-w-[750px] text-lg text-neutral-600 dark:text-neutral-400 sm:text-xl">
         {description}
       </span>
+      <Chat />
     </div>
   </div>
 );
