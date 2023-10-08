1ba867c29ac9eef04f771181ff3caef4d2d8aecd
Hayden Bleasel <hello@haydenbleasel.com>
2023-10-07 22:23:04 -0400

    Scaffold shadcn/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,36 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import typography from '@tailwindcss/typography';
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
-  darkMode: 'class',
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
   content: [
-    './app/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
-    './node_modules/@beskar-labs/gravity/**/*.js',
-  ],
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      fontFamily: {
-        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
-        mono: ['var(--font-mono)', ...defaultTheme.fontFamily.mono],
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [typography],
-};
-
-export default config;
+  plugins: [require("tailwindcss-animate")],
+}
