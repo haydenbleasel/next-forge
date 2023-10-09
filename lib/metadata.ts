fc18c5f1c8adfbef7a6d724c06f080b52987464c
Hayden Bleasel <hello@haydenbleasel.com>
2023-10-08 21:07:22 -0400

    Deep-merge metadata

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,3 +1,4 @@
+import deepmerge from 'deepmerge';
 import type { Metadata } from 'next';
 
 type MetadataGenerator = Omit<Metadata, 'description' | 'title'> & {
@@ -20,6 +21,7 @@ export const createMetadata = ({
   description,
   path,
   image,
+  ...props
 }: MetadataGenerator): Metadata => {
   if (!process.env.NEXT_PUBLIC_SITE_URL) {
     throw new Error('NEXT_PUBLIC_SITE_URL is not defined');
@@ -27,41 +29,44 @@ export const createMetadata = ({
 
   const parsedTitle = `${title} | ${applicationName}`;
 
-  const metadata: Metadata = {
-    title: parsedTitle,
-    description,
-    applicationName,
-    authors: [author],
-    creator: author.name,
-    metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL),
-    formatDetection: {
-      telephone: false,
-    },
-    appleWebApp: {
-      capable: true,
-      statusBarStyle: 'default',
-      title: parsedTitle,
-    },
-    openGraph: {
+  const metadata: Metadata = deepmerge(
+    {
       title: parsedTitle,
       description,
-      type: 'website',
-      siteName: applicationName,
-      locale: 'en_US',
-      url: new URL(path ?? '/', process.env.NEXT_PUBLIC_SITE_URL).toString(),
+      applicationName,
+      authors: [author],
+      creator: author.name,
+      metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL),
+      formatDetection: {
+        telephone: false,
+      },
+      appleWebApp: {
+        capable: true,
+        statusBarStyle: 'default',
+        title: parsedTitle,
+      },
+      openGraph: {
+        title: parsedTitle,
+        description,
+        type: 'website',
+        siteName: applicationName,
+        locale: 'en_US',
+        url: new URL(path ?? '/', process.env.NEXT_PUBLIC_SITE_URL).toString(),
+      },
+      publisher,
+      twitter: {
+        card: 'summary_large_image',
+        creator: twitterHandle,
+      },
+      viewport: {
+        minimumScale: 1,
+        initialScale: 1,
+        width: 'device-width',
+        viewportFit: 'cover',
+      },
     },
-    publisher,
-    twitter: {
-      card: 'summary_large_image',
-      creator: twitterHandle,
-    },
-    viewport: {
-      minimumScale: 1,
-      initialScale: 1,
-      width: 'device-width',
-      viewportFit: 'cover',
-    },
-  };
+    props
+  );
 
   if (image && metadata.openGraph) {
     metadata.openGraph.images = [
