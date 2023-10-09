32e0dd2b30951aadea02b4c98a5b06b61b443116
Hayden Bleasel <hello@haydenbleasel.com>
2023-10-09 14:09:35 -0400

    Create copy command

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button';
 import { Notifications } from '@/components/notifications';
 import { ActiveProvider } from '@/components/activeProvider';
 import { pages } from '@/consts/navigation';
+import { Container } from '@/components/container';
 import { Logo } from './logo';
 import type { ReactElement } from 'react';
 
@@ -15,38 +16,40 @@ export const Navbar = async (): Promise<ReactElement> => {
   return (
     <div
       className={cn(
-        'flex items-center gap-8 backdrop-blur-sm justify-between border-b px-6 py-3',
+        'backdrop-blur-sm border-b py-3',
         'bg-white/90 border-zinc-200',
         'dark:bg-black/90 dark:border-zinc-800'
       )}
     >
-      <div className="flex items-center gap-4">
-        <Logo />
-        <div className="flex items-center">
-          {pages.map(({ name, href }) => (
-            <ActiveProvider key={name} href={href}>
-              <Button
-                asChild
-                variant="link"
-                className="group-[.active-page]:underline"
-              >
-                <Link href={href}>{name}</Link>
-              </Button>
-            </ActiveProvider>
-          ))}
+      <Container className="flex items-center gap-8 justify-between">
+        <div className="flex items-center gap-4">
+          <Logo />
+          <div className="flex items-center">
+            {pages.map(({ name, href }) => (
+              <ActiveProvider key={name} href={href}>
+                <Button
+                  asChild
+                  variant="link"
+                  className="group-[.active-page]:underline"
+                >
+                  <Link href={href}>{name}</Link>
+                </Button>
+              </ActiveProvider>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="flex items-center gap-2">
-        <ModeToggle />
-        <Notifications />
-        {user ? (
-          <UserButton afterSignOutUrl="/" />
-        ) : (
-          <Button asChild>
-            <Link href="/sign-in">Login</Link>
-          </Button>
-        )}
-      </div>
+        <div className="flex items-center gap-2">
+          <ModeToggle />
+          <Notifications />
+          {user ? (
+            <UserButton afterSignOutUrl="/" />
+          ) : (
+            <Button asChild>
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          )}
+        </div>
+      </Container>
     </div>
   );
 };
