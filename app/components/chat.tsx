5c50624505bb70f8ccea49dbad7c9d36a4f33549
Hayden Bleasel <hello@haydenbleasel.com>
2023-11-13 13:40:45 -0500

    Implement Vaul, add example into Chat

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -10,13 +10,82 @@ import {
 } from '@/components/ui/popover';
 import { cn } from '@/lib/utils';
 import { Input } from '@/components/ui/input';
+import { Drawer } from '@/components/drawer';
 import type { FC } from 'react';
 
-export const Chat: FC = () => {
+const ChatInner: FC<{ readonly className?: string }> = ({ className }) => {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div className="fixed bottom-4 right-4">
+    <div
+      className={cn(
+        'flex flex-1 flex-col divide-y h-full',
+        'bg-zinc-100 divide-zinc-200',
+        'dark:bg-zinc-950 dark:divide-zinc-800'
+      )}
+    >
+      <div
+        className={cn(
+          'flex-1 p-4 overflow-y-auto text-sm flex flex-col gap-2',
+          className
+        )}
+      >
+        {messages.map((message) => (
+          <div
+            key={message.id}
+            className={cn(
+              'max-w-[75%] px-4 py-2 rounded shadow-sm',
+              message.role === 'user'
+                ? 'bg-white self-start text-zinc-900'
+                : 'bg-black self-end text-white'
+            )}
+          >
+            {message.content}
+          </div>
+        ))}
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="shrink-0 flex flex-col gap-1 bg-white dark:bg-black"
+      >
+        <Input
+          value={input}
+          placeholder="Ask me something about next-forge"
+          onChange={handleInputChange}
+          className="rounded-none border-none shadow-none py-6 pb-16"
+        />
+        <div className="absolute bottom-0 right-0 p-4">
+          <Button type="submit" variant="outline" size="icon">
+            <PaperPlaneIcon />
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export const Chat: FC = () => (
+  <>
+    <div className="block sm:hidden">
+      <Drawer.Root>
+        <Drawer.Trigger asChild>
+          <div className="fixed bottom-4 right-4">
+            <Button size="icon">
+              <ChatBubbleIcon />
+            </Button>
+          </div>
+        </Drawer.Trigger>
+        <Drawer.Portal>
+          <Drawer.Content className="p-0">
+            <Drawer.Handle />
+            <ChatInner className="pt-12" />
+          </Drawer.Content>
+          <Drawer.Overlay />
+        </Drawer.Portal>
+      </Drawer.Root>
+    </div>
+    <div className="hidden sm:block fixed bottom-4 right-4">
       <Popover>
         <PopoverTrigger asChild>
           <Button size="icon">
@@ -24,48 +93,13 @@ export const Chat: FC = () => {
           </Button>
         </PopoverTrigger>
         <PopoverContent
-          className={cn(
-            'h-[50vh] w-[24rem] flex flex-col divide-y p-0 overflow-hidden',
-            'bg-zinc-100 divide-zinc-200',
-            'dark:bg-zinc-950 dark:divide-zinc-800'
-          )}
+          className="h-[50vh] w-[24rem] p-0 overflow-hidden"
           collisionPadding={16}
           sideOffset={16}
         >
-          <div className="flex-1 p-4 overflow-y-auto text-sm flex flex-col gap-2">
-            {messages.map((message) => (
-              <div
-                key={message.id}
-                className={cn(
-                  'max-w-[75%] px-4 py-2 rounded shadow-sm',
-                  message.role === 'user'
-                    ? 'bg-white self-start text-zinc-900'
-                    : 'bg-black self-end text-white'
-                )}
-              >
-                {message.content}
-              </div>
-            ))}
-          </div>
-
-          <form
-            onSubmit={handleSubmit}
-            className="shrink-0 flex flex-col gap-1 bg-white dark:bg-black"
-          >
-            <Input
-              value={input}
-              placeholder="Ask me something about next-forge"
-              onChange={handleInputChange}
-              className="rounded-none border-none shadow-none py-6 pb-16"
-            />
-            <div className="absolute bottom-0 right-0 p-4">
-              <Button type="submit" variant="outline" size="icon">
-                <PaperPlaneIcon />
-              </Button>
-            </div>
-          </form>
+          <ChatInner />
         </PopoverContent>
       </Popover>
     </div>
-  );
-};
+  </>
+);
