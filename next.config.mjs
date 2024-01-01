72ff3a12d810846474d1916c64e52d3b2188a0d0
Hayden Bleasel <hello@haydenbleasel.com>
2024-01-01 15:25:24 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ import { withLogtail } from '@logtail/next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     formats: ['image/avif', 'image/webp'],
   },
