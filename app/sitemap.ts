3828992433bf6245c71975a3c1a951e9f0d3d778
Hayden Bleasel <hello@haydenbleasel.com>
2024-04-01 18:51:08 -0400

    Remove hidden and route groups from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,16 +6,22 @@ import type { MetadataRoute } from 'next';
 const appFolders = fs.readdirSync('app', { withFileTypes: true });
 const pages = appFolders
   .filter((file) => file.isDirectory())
+  .filter((folder) => !folder.name.startsWith('_'))
+  .filter((folder) => !folder.name.startsWith('('))
   .map((folder) => folder.name);
 
 const blogs = fs
   .readdirSync('content/blog', { withFileTypes: true })
   .filter((file) => !file.isDirectory())
+  .filter((file) => !file.name.startsWith('_'))
+  .filter((file) => !file.name.startsWith('('))
   .map((file) => file.name.replace('.mdx', ''));
 
 const legals = fs
   .readdirSync('content/legal', { withFileTypes: true })
   .filter((file) => !file.isDirectory())
+  .filter((file) => !file.name.startsWith('_'))
+  .filter((file) => !file.name.startsWith('('))
   .map((file) => file.name.replace('.mdx', ''));
 
 export default function sitemap(): MetadataRoute.Sitemap {
