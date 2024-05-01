2a9cb1186ffd81f54e0e0aab449abc1337dd578a
Hayden Bleasel <hello@haydenbleasel.com>
2024-05-01 13:14:25 -0400

    Cleanup deps

--- packages/design-system/components/mode-toggle.tsx
+++ packages/design-system/components/mode-toggle.tsx
@@ -3,13 +3,13 @@
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { useTheme } from 'next-themes';
 
-import { Button } from '@repo/design-system/components/ui/button';
+import { Button } from './ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from '@repo/design-system/components/ui/dropdown-menu';
+} from './ui/dropdown-menu';
 import type { FC } from 'react';
 
 export const ModeToggle: FC = () => {
