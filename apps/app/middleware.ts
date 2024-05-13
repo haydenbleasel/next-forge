0b95cd72270ecdbd12dcd41bd060a5cd1e9eef11
Hayden Bleasel <hello@haydenbleasel.com>
2024-05-13 11:17:54 -0400

    Update middleware.ts

--- apps/app/middleware.ts
+++ apps/app/middleware.ts
@@ -1,7 +1,17 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-export default clerkMiddleware(() => NextResponse.next());
+const isProtectedRoute = createRouteMatcher([
+  /^(?!\/(?<path>sign-in|sign-up)).*$/iu,
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) {
+    auth().protect();
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
