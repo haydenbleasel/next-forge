0b799fa38f8541e3a01f20fe317d8d73d3c9f62d
Hayden Bleasel <hello@haydenbleasel.com>
2024-06-07 14:57:40 +1000

    Fix duplicate tags

--- apps/app/app/layout.tsx
+++ apps/app/app/layout.tsx
@@ -1,8 +1,6 @@
 import '@repo/design-system/styles/globals.css';
-import { Analytics } from '@vercel/analytics/react';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
-import { Toaster } from '@repo/design-system/components/ui/sonner';
 import { TooltipProvider } from '@repo/design-system/components/ui/tooltip';
 import { DesignSystemProvider } from '@repo/design-system/provider';
 import { cn } from '@repo/design-system/lib/utils';
@@ -25,8 +23,6 @@ const RootLayout: FC<RootLayoutProperties> = ({ children }) => (
       <DesignSystemProvider>
         <TooltipProvider>{children}</TooltipProvider>
       </DesignSystemProvider>
-      <Toaster />
-      <Analytics />
     </body>
   </html>
 );
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,6 @@
 import '@repo/design-system/styles/globals.css';
-import { Analytics } from '@vercel/analytics/react';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
-import { Toaster } from '@repo/design-system/components/ui/sonner';
 import { TooltipProvider } from '@repo/design-system/components/ui/tooltip';
 import { DesignSystemProvider } from '@repo/design-system/provider';
 import { cn } from '@repo/design-system/lib/utils';
@@ -31,8 +29,6 @@ const RootLayout: FC<RootLayoutProperties> = ({ children }) => (
           <Footer />
         </TooltipProvider>
       </DesignSystemProvider>
-      <Toaster />
-      <Analytics />
     </body>
   </html>
 );
