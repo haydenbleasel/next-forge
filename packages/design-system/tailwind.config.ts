19716be366708e3e853a824bb1e31fccd50a3e12
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-21 11:51:17 -0400

    Fix CSS vars

--- packages/design-system/tailwind.config.ts
+++ packages/design-system/tailwind.config.ts
@@ -7,7 +7,7 @@ import typographyConfig from './typography.config';
 const config: Config = {
   darkMode: ['class'],
   content: [
-    'node_modules/@repo/design-system/**/*.{ts,tsx}',
+    './node_modules/@repo/design-system/components/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './lib/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
