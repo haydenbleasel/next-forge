713951fecc9ee7b2f5452d7fe4f01394e22a3279
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-21 23:31:01 -0400

    Simplify app

--- apps/app/instrumentation.ts
+++ apps/app/instrumentation.ts
@@ -1,11 +1,15 @@
-/* eslint-disable import/dynamic-import-chunkname */
+import * as Sentry from '@sentry/nextjs';
 
-export const register = async (): Promise<void> => {
+export const register = () => {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config');
+    Sentry.init({
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    });
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config');
+    Sentry.init({
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    });
   }
 };
