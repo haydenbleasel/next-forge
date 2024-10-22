0370c3e4db5c7ed14c1c9782d74607ed02bd01b0
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-22 13:09:48 -0400

    Apply shared next-config globally

---
+++ apps/web/next.config.ts
@@ -0,0 +1,23 @@
+import { config, withAnalyzer, withSentry } from '@repo/next-config';
+import type { NextConfig } from 'next';
+
+let nextConfig: NextConfig = { ...config };
+
+if (process.env.NODE_ENV === 'production') {
+  nextConfig.rewrites = async () => [
+    {
+      source: '/legal',
+      destination: '/legal/privacy',
+    },
+  ];
+}
+
+if (process.env.VERCEL) {
+  nextConfig = withSentry(nextConfig);
+}
+
+if (process.env.ANALYZE === 'true') {
+  nextConfig = withAnalyzer(nextConfig);
+}
+
+export default nextConfig;
