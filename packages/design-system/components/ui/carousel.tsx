f0f74d5caae9b4ad514492e9e9a76178b013e96b
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-22 12:56:04 -0400

    Update carousel.tsx

--- packages/design-system/components/ui/carousel.tsx
+++ packages/design-system/components/ui/carousel.tsx
@@ -7,7 +7,7 @@ import useEmblaCarousel, {
 } from "embla-carousel-react"
 
 import { cn } from "@repo/design-system/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Button } from "@repo/design-system/components/ui/button"
 
 type CarouselApi = UseEmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
