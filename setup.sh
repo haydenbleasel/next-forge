17369f37aa3f1134f7bf72f565de73cf0efbe48e
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-28 14:35:58 -0400

    Finish mocking setup

--- setup.sh
+++ setup.sh
@@ -2,48 +2,6 @@
 
 echo "Setting up next-forge..."
 
-# Install Homebrew
-if which brew >/dev/null; then
-  echo "Homebrew is installed. Updating..."
-  brew update
-else
-  echo "Homebrew is not installed. Installing..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# Install NVM
-if which nvm >/dev/null; then
-  echo "NVM is installed."
-else
-  echo "NVM is not installed. Installing..."
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-  nvm install node
-fi
-
-# Install pnpm
-if which pnpm >/dev/null; then
-  echo "pnpm is installed."
-else
-  echo "pnpm is not installed. Installing..."
-  brew install pnpm
-fi
-
-# Install neonctl
-if which neonctl >/dev/null; then
-  echo "neonctl is installed."
-else
-  echo "neonctl is not installed. Installing..."
-  brew install neonctl
-fi
-
-# Install Stripe
-if which stripe >/dev/null; then
-  echo "Stripe is installed."
-else
-  echo "Stripe is not installed. Installing..."
-  brew install stripe/stripe-cli/stripe
-fi
-
 # Install dependencies
 echo "Installing dependencies..."
 pnpm install
@@ -57,7 +15,7 @@ cp apps/studio/.env.example apps/studio/.env
 
 # Delete demo folder
 echo "Deleting demo folder..."
-rm -rf apps/demo
+rm -rf apps/_docs
 
 # Finish setup
 echo "Setup complete! Deleting setup script..."
