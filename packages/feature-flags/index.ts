125789435cad7fd30d131fc9ff0487bfad635a6f
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-28 19:09:56 -0400

    Scaffold basic posthog-backed flag

--- packages/feature-flags/index.ts
+++ packages/feature-flags/index.ts
@@ -1,6 +1,18 @@
+import { currentUser } from '@clerk/nextjs/server';
+import { analytics } from '@repo/design-system/lib/analytics/server';
 import { unstable_flag as flag } from '@vercel/flags/next';
 
 export const showBetaFeature = flag({
   key: 'beta-feature',
-  decide: () => false,
+  async decide() {
+    const user = await currentUser();
+
+    if (!user) {
+      return false;
+    }
+
+    const isEnabled = await analytics.isFeatureEnabled(this.key, user.id);
+
+    return isEnabled ?? false;
+  },
 });
