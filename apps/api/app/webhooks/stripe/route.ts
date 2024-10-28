d9b9c86b9867d45686a6d0d0e070cdb5998fd185
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-28 18:34:43 -0400

    Prepare Stripe route for analytics

--- apps/api/app/webhooks/stripe/route.ts
+++ apps/api/app/webhooks/stripe/route.ts
@@ -1,10 +1,27 @@
 import { log } from '@logtail/next';
+import { analytics } from '@repo/design-system/lib/analytics/server';
 import { parseError } from '@repo/design-system/lib/error';
 import { stripe } from '@repo/design-system/lib/stripe';
 import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 
-const secret = process.env.STRIPE_WEBHOOK_SECRET ?? '';
+const secret = process.env.STRIPE_WEBHOOK_SECRET;
+
+if (!secret) {
+  throw new Error('STRIPE_WEBHOOK_SECRET is not set');
+}
+
+const handleCheckoutSessionCompleted = (data: object) => {
+  log.info('checkout.session.completed', { data });
+};
+
+const handlePaymentIntentSucceeded = (data: object) => {
+  log.info('payment_intent.succeeded', { data });
+};
+
+const handlePaymentIntentPaymentFailed = (data: object) => {
+  log.info('payment_intent.payment_failed', { data });
+};
 
 export const POST = async (request: Request): Promise<Response> => {
   try {
@@ -20,21 +37,23 @@ export const POST = async (request: Request): Promise<Response> => {
 
     switch (event.type) {
       case 'checkout.session.completed': {
-        log.info('checkout.session.completed');
+        await handleCheckoutSessionCompleted(event.data.object);
         break;
       }
       case 'payment_intent.succeeded': {
-        log.info('payment_intent.succeeded');
+        await handlePaymentIntentSucceeded(event.data.object);
         break;
       }
       case 'payment_intent.payment_failed': {
-        log.info('payment_intent.payment_failed');
+        await handlePaymentIntentPaymentFailed(event.data.object);
         break;
       }
       default: {
         log.warn(`Unhandled event type ${event.type}`);
       }
     }
+
+    await analytics.shutdown();
 
     return NextResponse.json({ result: event, ok: true });
   } catch (error) {
