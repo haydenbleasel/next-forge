f40a02c871aa4f6b2c7b51cabdbfca419e5a0298
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-28 19:57:01 -0400

    Add Vercel toolbar overrides

---
+++ packages/feature-flags/access.ts
@@ -0,0 +1,28 @@
+import { type ApiData, verifyAccess } from '@vercel/flags';
+import { type NextRequest, NextResponse } from 'next/server';
+import * as flags from './index';
+
+export const getFlags = async (request: NextRequest) => {
+  const access = await verifyAccess(request.headers.get('Authorization'));
+
+  if (!access) {
+    return NextResponse.json(null, { status: 401 });
+  }
+
+  const definitions = Object.fromEntries(
+    Object.entries(flags).map(([key, flag]) => [
+      key,
+      {
+        origin: flag.origin,
+        description: flag.description,
+        options: flag.options,
+      },
+    ])
+  );
+
+  console.log(definitions, 'definitions');
+
+  return NextResponse.json<ApiData>({
+    definitions,
+  });
+};
--- packages/feature-flags/index.ts
+++ packages/feature-flags/index.ts
@@ -1,10 +1,21 @@
 import { auth } from '@clerk/nextjs/server';
 import { analytics } from '@repo/design-system/lib/analytics/server';
+import { type FlagOverridesType, decrypt } from '@vercel/flags';
 import { unstable_flag as flag } from '@vercel/flags/next';
 
 export const showBetaFeature = flag({
-  key: 'beta-feature',
-  async decide() {
+  key: 'showBetaFeature',
+  async decide(params) {
+    const { key } = this;
+    const overrideCookie = params.cookies.get('vercel-flag-overrides')?.value;
+    const overrides = overrideCookie
+      ? await decrypt<FlagOverridesType>(overrideCookie)
+      : {};
+
+    if (overrides && key in overrides) {
+      return overrides[key];
+    }
+
     try {
       const { userId } = await auth();
 
@@ -12,7 +23,7 @@ export const showBetaFeature = flag({
         return false;
       }
 
-      const isEnabled = await analytics.isFeatureEnabled(this.key, userId);
+      const isEnabled = await analytics.isFeatureEnabled(key, userId);
 
       return isEnabled ?? false;
     } catch (_error) {
