a0c095e074edd40627a7e6dff60064a40c236c06
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-28 12:11:05 -0400

    Remove Banner

--- apps/_docs/app/layout.tsx
+++ apps/_docs/app/layout.tsx
@@ -1,5 +1,4 @@
 import './global.css';
-import { Banner } from 'fumadocs-ui/components/banner';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
@@ -12,7 +11,6 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <Banner>Hello World</Banner>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
