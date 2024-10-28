ac54b34af8038c735ff652448cec71e435c13ea4
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-28 17:56:00 -0400

    Add posthog provider to design system

--- packages/design-system/provider.tsx
+++ packages/design-system/provider.tsx
@@ -2,7 +2,6 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { ThemeProviderProps } from 'next-themes/dist/types';
-import type { FC } from 'react';
 import { Toaster } from './components/ui/sonner';
 import { TooltipProvider } from './components/ui/tooltip';
 
@@ -10,22 +9,22 @@ const gaMeasurementId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
 type DesignSystemProviderProperties = ThemeProviderProps;
 
-export const DesignSystemProvider = (
-  {
-    children,
-    ...properties
-  }: DesignSystemProviderProperties
-) => (<>
-  <NextThemesProvider
-    attribute="class"
-    defaultTheme="system"
-    enableSystem
-    disableTransitionOnChange
-    {...properties}
-  >
-    <TooltipProvider>{children}</TooltipProvider>
-  </NextThemesProvider>
-  <Toaster />
-  <VercelAnalytics />
-  {gaMeasurementId && <GoogleAnalytics gaId={gaMeasurementId} />}
-</>);
+export const DesignSystemProvider = ({
+  children,
+  ...properties
+}: DesignSystemProviderProperties) => (
+  <>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      {...properties}
+    >
+      <TooltipProvider>{children}</TooltipProvider>
+    </NextThemesProvider>
+    <Toaster />
+    <VercelAnalytics />
+    {gaMeasurementId && <GoogleAnalytics gaId={gaMeasurementId} />}
+  </>
+);
