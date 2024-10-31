ec07e48509a7edec0cf81d5017a97e005c7b454b
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-31 10:19:55 -0400

    Fix dark mode on website

--- apps/web/app/components/footer.tsx
+++ apps/web/app/components/footer.tsx
@@ -44,68 +44,72 @@ export const Footer = () => {
   ];
 
   return (
-    <div className="w-full bg-foreground py-20 text-background lg:py-40">
-      <div className="container mx-auto">
-        <div className="grid items-center gap-10 lg:grid-cols-2">
-          <div className="flex flex-col items-start gap-8">
-            <div className="flex flex-col gap-2">
-              <h2 className="max-w-xl text-left font-regular text-3xl tracking-tighter md:text-5xl">
-                next-forge
-              </h2>
-              <p className="max-w-lg text-left text-background/75 text-lg leading-relaxed tracking-tight">
-                This is the start of something new.
-              </p>
-            </div>
-            <Status />
-          </div>
-          <div className="grid items-start gap-10 lg:grid-cols-3">
-            {navigationItems.map((item) => (
-              <div
-                key={item.title}
-                className="flex flex-col items-start gap-1 text-base"
-              >
-                <div className="flex flex-col gap-2">
-                  {item.href ? (
-                    <Link
-                      href={item.href}
-                      className="flex items-center justify-between"
-                      target={item.href.includes('http') ? '_blank' : undefined}
-                      rel={
-                        item.href.includes('http')
-                          ? 'noopener noreferrer'
-                          : undefined
-                      }
-                    >
-                      <span className="text-xl">{item.title}</span>
-                    </Link>
-                  ) : (
-                    <p className="text-xl">{item.title}</p>
-                  )}
-                  {item.items?.map((subItem) => (
-                    <Link
-                      key={subItem.title}
-                      href={subItem.href}
-                      className="flex items-center justify-between"
-                      target={
-                        subItem.href.includes('http') ? '_blank' : undefined
-                      }
-                      rel={
-                        subItem.href.includes('http')
-                          ? 'noopener noreferrer'
-                          : undefined
-                      }
-                    >
-                      <span className="text-background/75">
-                        {subItem.title}
-                      </span>
-                    </Link>
-                  ))}
-                </div>
+    <section className="dark border-t border-foreground/10">
+      <div className="w-full bg-background py-20 text-foreground lg:py-40">
+        <div className="container mx-auto">
+          <div className="grid items-center gap-10 lg:grid-cols-2">
+            <div className="flex flex-col items-start gap-8">
+              <div className="flex flex-col gap-2">
+                <h2 className="max-w-xl text-left font-regular text-3xl tracking-tighter md:text-5xl">
+                  next-forge
+                </h2>
+                <p className="max-w-lg text-left text-foreground/75 text-lg leading-relaxed tracking-tight">
+                  This is the start of something new.
+                </p>
               </div>
-            ))}
+              <Status />
+            </div>
+            <div className="grid items-start gap-10 lg:grid-cols-3">
+              {navigationItems.map((item) => (
+                <div
+                  key={item.title}
+                  className="flex flex-col items-start gap-1 text-base"
+                >
+                  <div className="flex flex-col gap-2">
+                    {item.href ? (
+                      <Link
+                        href={item.href}
+                        className="flex items-center justify-between"
+                        target={
+                          item.href.includes('http') ? '_blank' : undefined
+                        }
+                        rel={
+                          item.href.includes('http')
+                            ? 'noopener noreferrer'
+                            : undefined
+                        }
+                      >
+                        <span className="text-xl">{item.title}</span>
+                      </Link>
+                    ) : (
+                      <p className="text-xl">{item.title}</p>
+                    )}
+                    {item.items?.map((subItem) => (
+                      <Link
+                        key={subItem.title}
+                        href={subItem.href}
+                        className="flex items-center justify-between"
+                        target={
+                          subItem.href.includes('http') ? '_blank' : undefined
+                        }
+                        rel={
+                          subItem.href.includes('http')
+                            ? 'noopener noreferrer'
+                            : undefined
+                        }
+                      >
+                        <span className="text-foreground/75">
+                          {subItem.title}
+                        </span>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
