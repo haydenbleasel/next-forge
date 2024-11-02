38fb856ed0c7e1c9c852ada6c02b01c434629fb5
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-02 11:31:30 -0400

    Improve auth pages

--- packages/design-system/lib/consts.ts
+++ packages/design-system/lib/consts.ts
@@ -1,11 +1,16 @@
 const urls = {
   baseUrl: process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
+  webUrl: process.env.NEXT_PUBLIC_WEB_URL,
   appUrl: process.env.NEXT_PUBLIC_APP_URL,
   docsUrl: process.env.NEXT_PUBLIC_DOCS_URL,
 };
 
 if (!urls.baseUrl) {
   throw new Error('NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL is not set');
+}
+
+if (!urls.webUrl) {
+  throw new Error('NEXT_PUBLIC_WEB_URL is not set');
 }
 
 if (!urls.appUrl) {
@@ -19,5 +24,4 @@ if (!urls.docsUrl) {
 export const baseUrl = urls.baseUrl
   ? `https://${urls.baseUrl}`
   : 'http://localhost:3000';
-export const { appUrl } = urls;
-export const { docsUrl } = urls;
+export const { appUrl, webUrl, docsUrl } = urls;
