f8feb6b263bf0b34ae547cdbbad6799d590c8e49
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-03 11:22:15 -0500

    Remove testing package

--- apps/app/vitest.config.ts
+++ apps/app/vitest.config.ts
@@ -1,1 +1,9 @@
-export { default } from '@repo/testing';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+  },
+});
