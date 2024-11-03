b6c700d0bc7851d64a76eeef407d178670d45045
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-03 11:35:38 -0500

    Update stripe.ts

--- packages/design-system/lib/stripe.ts
+++ packages/design-system/lib/stripe.ts
@@ -7,5 +7,5 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-09-30.acacia',
+  apiVersion: '2024-10-28.acacia',
 });
