2e0211a3d065a3843c3cafe23be93a14087f146b
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-06 08:31:11 +0900

    For #180

--- packages/design-system/providers/posthog.tsx
+++ packages/design-system/providers/posthog.tsx
@@ -4,22 +4,6 @@ import { PostHogProvider as PostHogProviderRaw } from 'posthog-js/react';
 import type { ReactNode } from 'react';
 import { analytics } from '../lib/analytics/client';
 
-const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST;
-
-if (!posthogKey || !posthogHost) {
-  throw new Error(
-    'NEXT_PUBLIC_POSTHOG_KEY and NEXT_PUBLIC_POSTHOG_HOST must be set'
-  );
-}
-
-if (typeof window !== 'undefined') {
-  analytics.init(posthogKey, {
-    api_host: posthogHost,
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-  });
-}
-
 export const PostHogProvider = ({ children }: { children: ReactNode }) => (
   <PostHogProviderRaw client={analytics}>{children}</PostHogProviderRaw>
 );
