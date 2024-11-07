7691ee13f1bb0582ff51c3119cd25f816fdf67b8
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-07 18:11:09 +0900

    Update attribution.tsx

--- apps/_docs/app/(home)/components/attribution.tsx
+++ apps/_docs/app/(home)/components/attribution.tsx
@@ -1,9 +1,7 @@
 export const Attribution = () => (
   <div className="flex items-center justify-center p-4">
     <p className="text-center text-muted-foreground text-xs">
-      <a href="https://logo.dev" alt="Logo API">
-        Logos provided by Logo.dev
-      </a>
+      <a href="https://logo.dev">Logos provided by Logo.dev</a>
     </p>
   </div>
 );
