0aeb0bb98d248121011dc16659d7113cac650d04
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-09 17:57:35 +0900

    Update scripts

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,25 +1,22 @@
 @echo off
+
 echo Setting up next-forge...
 
-:: Install dependencies
 echo Installing dependencies...
 pnpm install
 
-:: Duplicate .env.example files
 echo Copying .env.example files to .env.local...
 copy apps\api\.env.example apps\api\.env.local
 copy apps\app\.env.example apps\app\.env.local
 copy apps\web\.env.example apps\web\.env.local
 copy packages\database\.env.example packages\database\.env
 
-:: Delete demo folder
-echo Deleting demo folder...
-rmdir /S /Q apps\_docs
+echo Deleting demo folders...
+rmdir /S /Q splash
+rmdir /S /Q docs
 
-:: Setup Prisma
 echo Setting up Prisma...
 pnpm build --filter @repo/database
 
-:: Finish setup
 echo Setup complete! Deleting setup folder...
 rmdir /S /Q scripts
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,25 +2,22 @@
 
 echo "Setting up next-forge..."
 
-# Install dependencies
 echo "Installing dependencies..."
 pnpm install
 
-# Duplicate .env.example files
 echo "Copying .env.example files to .env.local..."
 cp apps/api/.env.example apps/api/.env.local
 cp apps/app/.env.example apps/app/.env.local
 cp apps/web/.env.example apps/web/.env.local
 cp packages/database/.env.example packages/database/.env
 
-# Delete demo folder
-echo "Deleting demo folder..."
-rm -rf apps/_docs
+echo "Deleting demo folders..."
+rm -rf splash
+rm -rf docs
 
 echo "Setting up Prisma..."
 pnpm build --filter @repo/database
 
-# Finish setup
 echo "Setup complete! Deleting setup folder..."
 rm -rf scripts
 
