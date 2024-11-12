0f58eeb2fbe3920c2c5e123c8cc0a0fcc7ff67b4
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-12 12:43:29 -0500

    Misc fixes

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -11,12 +11,20 @@ copy apps\app\.env.example apps\app\.env.local
 copy apps\web\.env.example apps\web\.env.local
 copy packages\database\.env.example packages\database\.env
 
-echo Deleting demo folders...
-rmdir /S /Q splash
+echo Deleting internal content...
+rmdir /S /Q .github/workflows
+del .github/CONTRIBUTING.md
+del .github/FUNDING.md
+del .github/SECURITY.md
 rmdir /S /Q docs
+rmdir /S /Q splash
+del .autorc
+del .coderabbit.yaml
+del CHANGELOG.md
+del license.md
 
 echo Setting up Prisma...
 pnpm build --filter @repo/database
 
-echo Setup complete! Deleting setup folder...
+echo Setup complete! Deleting scripts folder...
 rmdir /S /Q scripts
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,13 +11,21 @@ cp apps/app/.env.example apps/app/.env.local
 cp apps/web/.env.example apps/web/.env.local
 cp packages/database/.env.example packages/database/.env
 
-echo "Deleting demo folders..."
-rm -rf splash
+echo "Deleting internal content..."
+rm -rf .github/workflows
+rm .github/CONTRIBUTING.md
+rm .github/FUNDING.md
+rm .github/SECURITY.md
 rm -rf docs
+rm -rf splash
+rm .autorc
+rm .coderabbit.yaml
+rm CHANGELOG.md
+rm license.md
 
 echo "Setting up Prisma..."
 pnpm build --filter @repo/database
 
-echo "Setup complete! Deleting setup folder..."
+echo "Setup complete! Deleting scripts folder..."
 rm -rf scripts
 
