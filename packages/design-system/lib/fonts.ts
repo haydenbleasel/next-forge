4b1f64bd7e4df33ca813097ce2ece760c14abc30
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-12 20:31:06 +0900

    Update fonts.ts

--- packages/design-system/lib/fonts.ts
+++ packages/design-system/lib/fonts.ts
@@ -1,10 +1,18 @@
 import { cn } from '@repo/design-system/lib/utils';
 import { Geist, Geist_Mono } from 'next/font/google';
 
-const sans = Geist({ subsets: ['latin'], variable: '--font-sans' });
+const sans = Geist({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  display: 'swap',
+  weight: 'variable',
+});
+
 const mono = Geist_Mono({
   subsets: ['latin'],
   variable: '--font-mono',
+  display: 'swap',
+  weight: 'variable',
 });
 
 export const fonts = cn(
