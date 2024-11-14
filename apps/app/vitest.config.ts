638fd7d926d1464cac2c47d3c4198ec0d79fcab9
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-14 08:47:46 -0500

    Update vitest.config.ts

--- apps/app/vitest.config.ts
+++ apps/app/vitest.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
@@ -5,5 +6,11 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+      '@repo': path.resolve(__dirname, '../../packages'),
+    },
   },
 });
