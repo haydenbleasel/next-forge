d122bc41d5a3708d7b322baf57e0dc8744f5ba29
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-17 12:13:54 -0500

    Update image, add more colors

--- apps/app/app/(authenticated)/components/cursors.tsx
+++ apps/app/app/(authenticated)/components/cursors.tsx
@@ -48,10 +48,22 @@ const Cursor = ({
 
 const COLORS = [
   tailwind.theme.colors.red[500],
-  tailwind.theme.colors.blue[500],
-  tailwind.theme.colors.green[500],
+  tailwind.theme.colors.orange[500],
+  tailwind.theme.colors.amber[500],
   tailwind.theme.colors.yellow[500],
+  tailwind.theme.colors.lime[500],
+  tailwind.theme.colors.green[500],
+  tailwind.theme.colors.emerald[500],
+  tailwind.theme.colors.teal[500],
+  tailwind.theme.colors.cyan[500],
+  tailwind.theme.colors.sky[500],
+  tailwind.theme.colors.blue[500],
+  tailwind.theme.colors.indigo[500],
+  tailwind.theme.colors.violet[500],
   tailwind.theme.colors.purple[500],
+  tailwind.theme.colors.fuchsia[500],
+  tailwind.theme.colors.pink[500],
+  tailwind.theme.colors.rose[500],
 ];
 
 export const Cursors = () => {
