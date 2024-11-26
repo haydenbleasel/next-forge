f756f528ee8f793a74c0a4057746d57e02ef32d7
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-25 20:33:11 -0500

    Bump deps

--- packages/payments/index.ts
+++ packages/payments/index.ts
@@ -3,7 +3,7 @@ import { env } from '@repo/env';
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-10-28.acacia',
+  apiVersion: '2024-11-20.acacia',
 });
 
 export type { Stripe } from 'stripe';
