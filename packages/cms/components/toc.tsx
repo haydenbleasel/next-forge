36f7108bc52dc57ceeee088dd93964e3885d0b13
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-08 13:41:44 -0500

    Start working on TOC

--- packages/cms/components/toc.tsx
+++ packages/cms/components/toc.tsx
@@ -1,26 +1,25 @@
+import { Body } from './body';
+
 type TableOfContentsProperties = {
   readonly data: object;
 };
 
 export const TableOfContents = ({ data }: TableOfContentsProperties) => (
   <div>
-    <pre>{JSON.stringify(data, null, 2)}</pre>
-    {/* <ul className="flex list-none flex-col gap-2 text-sm">
-      {data.map((item) => (
-        <li
-          key={item.url}
-          style={{
-            paddingLeft: `${item.depth - 2}rem`,
-          }}
-        >
+    <Body
+      blocks={data}
+      components={{
+        ol: (props) => (
+          <ol className="flex list-none flex-col gap-2 text-sm" {...props} />
+        ),
+        li: ({ children }) => <li className="pl-3">{children}</li>,
+        a: (props) => (
           <a
-            href={item.url}
             className="line-clamp-3 flex rounded-sm text-foreground text-sm underline decoration-foreground/0 transition-colors hover:decoration-foreground/50"
-          >
-            {item._title}
-          </a>
-        </li>
-      ))}
-    </ul> */}
+            {...props}
+          />
+        ),
+      }}
+    />
   </div>
 );
