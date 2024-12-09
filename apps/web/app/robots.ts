961f153448594fc00a24577992937295cc4b04be
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-09 14:28:21 -0500

    Move VERCEL_PROJECT_PRODUCTION_URL to Server

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -7,9 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: new URL(
-      '/sitemap.xml',
-      env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-    ).href,
+    sitemap: new URL('/sitemap.xml', env.VERCEL_PROJECT_PRODUCTION_URL).href,
   };
 }
