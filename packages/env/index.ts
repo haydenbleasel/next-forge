fb87c423c0b4f9088de86479fbdb7ad821ebf1ff
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-09 15:10:08 -0500

    Start breaking apart mono-env

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,42 +1,31 @@
-import { keys as ai } from '@repo/ai/keys';
-import { keys as analytics } from '@repo/analytics/keys';
-import { keys as auth } from '@repo/auth/keys';
-import { keys as cms } from '@repo/cms/keys';
-import { keys as collaboration } from '@repo/collaboration/keys';
-import { keys as database } from '@repo/database/keys';
-import { keys as email } from '@repo/email/keys';
-import { keys as flags } from '@repo/feature-flags/keys';
-import { keys as observability } from '@repo/observability/keys';
-import { keys as payments } from '@repo/payments/keys';
-import { keys as security } from '@repo/security/keys';
-import { keys as storage } from '@repo/storage/keys';
-import { keys as webhooks } from '@repo/webhooks/keys';
 import { vercel } from '@t3-oss/env-core/presets';
 import { createEnv } from '@t3-oss/env-nextjs';
-import { core } from './core';
+import { z } from 'zod';
 
 // UPSTASH_REDIS_REST_URL: z.string().min(1).url().optional(),
 // UPSTASH_REDIS_REST_TOKEN: z.string().min(1).optional(),
 
-export const env = createEnv({
-  extends: [
-    core(),
-    vercel(),
-    auth(),
-    ai(),
-    analytics(),
-    cms(),
-    collaboration(),
-    database(),
-    email(),
-    flags(),
-    observability(),
-    payments(),
-    security(),
-    storage(),
-    webhooks(),
-  ],
-  server: {},
-  client: {},
-  runtimeEnv: {},
-});
+export const keys = () =>
+  createEnv({
+    extends: [vercel()],
+    server: {
+      ANALYZE: z.string().optional(),
+
+      // Added by Vercel
+      NEXT_RUNTIME: z.enum(['nodejs', 'edge']).optional(),
+    },
+    client: {
+      NEXT_PUBLIC_APP_URL: z.string().min(1).url(),
+      NEXT_PUBLIC_WEB_URL: z.string().min(1).url(),
+      NEXT_PUBLIC_API_URL: z.string().min(1).url().optional(),
+      NEXT_PUBLIC_DOCS_URL: z.string().min(1).url().optional(),
+    },
+    runtimeEnv: {
+      ANALYZE: process.env.ANALYZE,
+      NEXT_RUNTIME: process.env.NEXT_RUNTIME,
+      NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+      NEXT_PUBLIC_WEB_URL: process.env.NEXT_PUBLIC_WEB_URL,
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+      NEXT_PUBLIC_DOCS_URL: process.env.NEXT_PUBLIC_DOCS_URL,
+    },
+  });
