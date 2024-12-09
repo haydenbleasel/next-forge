d3705f079bafc48d4f530d66a44f5e9bf8a00fc6
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-09 10:32:18 -0500

    Create Header component for app

--- apps/app/app/(authenticated)/page.tsx
+++ apps/app/app/(authenticated)/page.tsx
@@ -1,21 +1,12 @@
 import { auth } from '@repo/auth/server';
 import { database } from '@repo/database';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@repo/design-system/components/ui/breadcrumb';
-import { Separator } from '@repo/design-system/components/ui/separator';
-import { SidebarTrigger } from '@repo/design-system/components/ui/sidebar';
 import { env } from '@repo/env';
 import type { Metadata } from 'next';
 import dynamic from 'next/dynamic';
 import { notFound } from 'next/navigation';
 import { AvatarStack } from './components/avatar-stack';
 import { Cursors } from './components/cursors';
+import { Header } from './components/header';
 
 const title = 'Acme Inc';
 const description = 'My application.';
@@ -41,31 +32,14 @@ const App = async () => {
 
   return (
     <>
-      <header className="flex h-16 shrink-0 items-center justify-between gap-2">
-        <div className="flex items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
+      <Header pages={['Building Your Application']} page="Data Fetching">
         {env.LIVEBLOCKS_SECRET && (
           <CollaborationProvider orgId={orgId}>
             <AvatarStack />
             <Cursors />
           </CollaborationProvider>
         )}
-      </header>
+      </Header>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
           {pages.map((page) => (
