e0b0bb728451ad69481e3719f6c3e3c190f08f5e
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-08 22:20:21 -0500

    Make security an optional addon

--- packages/security/index.ts
+++ packages/security/index.ts
@@ -1,19 +1,14 @@
-import arcjet, { shield } from '@arcjet/next';
+import arcjet, {
+  type ArcjetBotCategory,
+  type ArcjetWellKnownBot,
+  detectBot,
+  request,
+  shield,
+} from '@arcjet/next';
 import { env } from '@repo/env';
 
-// Re-export the rules to simplify imports inside handlers
-export {
-  detectBot,
-  fixedWindow,
-  protectSignup,
-  sensitiveInfo,
-  shield,
-  slidingWindow,
-  request,
-} from '@arcjet/next';
-
 // Create a base Arcjet instance which can be imported and extended in each route.
-export default arcjet({
+const base = arcjet({
   // Get your site key from https://app.arcjet.com
   key: env.ARCJET_KEY,
   // Identify the user by their IP address
@@ -21,8 +16,30 @@ export default arcjet({
   rules: [
     // Protect against common attacks with Arcjet Shield
     shield({
-      mode: 'LIVE', // will block requests. Use "DRY_RUN" to log only
+      // Will block requests. Use "DRY_RUN" to log only
+      mode: 'LIVE',
     }),
     // Other rules are added in different routes
   ],
 });
+
+export const secure = async (
+  allow: (ArcjetWellKnownBot | ArcjetBotCategory)[],
+  sourceRequest?: Request
+) => {
+  const req = sourceRequest ?? (await request());
+  const aj = base.withRule(detectBot({ mode: 'LIVE', allow }));
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    if (decision.reason.isBot()) {
+      throw new Error('No bots allowed');
+    }
+
+    if (decision.reason.isRateLimit()) {
+      throw new Error('Rate limit exceeded');
+    }
+
+    throw new Error('Access denied');
+  }
+};
