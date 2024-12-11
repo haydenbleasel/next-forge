43f44220d9e41891b22591183fd89b6ea2958373
David Mytton <david@arcjet.com>
2024-12-11 07:09:24 +0000

    Disable CSP

--- apps/app/middleware.ts
+++ apps/app/middleware.ts
@@ -11,45 +11,7 @@ import {
 // https://docs.arcjet.com/nosecone/quick-start
 const noseconeOptions: NoseconeOptions = {
   ...noseconeDefaults,
-  contentSecurityPolicy: {
-    ...noseconeDefaults.contentSecurityPolicy,
-    directives: {
-      ...noseconeDefaults.contentSecurityPolicy.directives,
-      scriptSrc: [
-        // We have to use unsafe-inline because next-themes and Vercel Analytics
-        // do not support nonce
-        // https://github.com/pacocoursey/next-themes/issues/106
-        // https://github.com/vercel/analytics/issues/122
-        //...noseconeDefaults.contentSecurityPolicy.directives.scriptSrc,
-        "'self'",
-        "'unsafe-inline'",
-        "https://www.googletagmanager.com",
-        "https://*.clerk.accounts.dev",
-        "https://va.vercel-scripts.com",
-      ],
-      connectSrc: [
-        ...noseconeDefaults.contentSecurityPolicy.directives.connectSrc,
-        "https://*.clerk.accounts.dev",
-        "https://*.google-analytics.com",
-        "https://clerk-telemetry.com",
-      ],
-      workerSrc: [
-        ...noseconeDefaults.contentSecurityPolicy.directives.workerSrc,
-        "blob:",
-        "https://*.clerk.accounts.dev"
-      ],
-      imgSrc: [
-        ...noseconeDefaults.contentSecurityPolicy.directives.imgSrc,
-        "https://img.clerk.com"
-      ],
-      objectSrc: [
-        ...noseconeDefaults.contentSecurityPolicy.directives.objectSrc,
-      ],
-      // We only set this in production because the server may be started
-      // without HTTPS
-      upgradeInsecureRequests: process.env.NODE_ENV === "production",
-    },
-  },
+  contentSecurityPolicy: false,
 }
 
 // Add Vercel toolbar compatible options if we're in a preview environment
