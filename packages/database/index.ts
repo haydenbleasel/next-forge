15243d78cd30fd7e22c4e70830c0f6888caa054b
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-12 18:04:14 -0500

    Rewrite docs a tad

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -3,9 +3,6 @@ import 'server-only';
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
-
-import { Redis } from '@upstash/redis';
-
 import { env } from '@repo/env';
 import ws from 'ws';
 
@@ -15,10 +12,5 @@ const pool = new Pool({ connectionString: env.DATABASE_URL });
 const adapter = new PrismaNeon(pool);
 
 export const database = new PrismaClient({ adapter });
-
-export const redis = new Redis({
-	url: env.UPSTASH_REDIS_REST_URL,
-	token: env.UPSTASH_REDIS_REST_TOKEN,
-})
 
 export * from '@prisma/client';
