4a86891e8919095253a4f48042c8791dedb472d2
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-15 14:50:59 -0500

    Update env.ts

--- apps/api/env.ts
+++ apps/api/env.ts
@@ -2,7 +2,7 @@ import { keys as analytics } from '@repo/analytics/keys';
 import { keys as auth } from '@repo/auth/keys';
 import { keys as database } from '@repo/database/keys';
 import { keys as email } from '@repo/email/keys';
-import { keys as core } from '@repo/env';
+import { keys as core } from '@repo/next-config/keys';
 import { keys as observability } from '@repo/observability/keys';
 import { keys as payments } from '@repo/payments/keys';
 import { createEnv } from '@t3-oss/env-nextjs';
