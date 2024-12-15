c6641cc18786cb1b5fca5b91355ddd69a6815958
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-15 14:50:48 -0500

    Move remaining env vars to next-config package

--- apps/web/env.ts
+++ apps/web/env.ts
@@ -1,7 +1,7 @@
 import { keys as cms } from '@repo/cms/keys';
 import { keys as email } from '@repo/email/keys';
-import { keys as core } from '@repo/env';
 import { keys as flags } from '@repo/feature-flags/keys';
+import { keys as core } from '@repo/next-config/keys';
 import { keys as observability } from '@repo/observability/keys';
 import { keys as rateLimit } from '@repo/rate-limit/keys';
 import { keys as security } from '@repo/security/keys';
