3e84a8b704c27d4d9221d215e80fd9bbaaac5337
Hayden Bleasel <hello@haydenbleasel.com>
2025-01-01 12:59:39 -0600

    Update Stripe API version

--- packages/payments/index.ts
+++ packages/payments/index.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 import { keys } from './keys';
 
 export const stripe = new Stripe(keys().STRIPE_SECRET_KEY, {
-  apiVersion: '2024-11-20.acacia',
+  apiVersion: '2024-12-18.acacia',
 });
 
 export type { Stripe } from 'stripe';
