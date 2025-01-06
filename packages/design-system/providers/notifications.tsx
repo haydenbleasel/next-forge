bb5b0d8803051b071e464969547b1ddb8b0784fc
Hayden Bleasel <hello@haydenbleasel.com>
2025-01-06 09:40:09 -0500

    Collab on Notifications (#388)
    * update env vars
    
    * update notif provider
    
    * add secret key for server sdk
    
    * create feed component
    
    * make provider client
    
    ---------
    
    Co-authored-by: Jeff Everhart <jeffeverhart383@gmail.com>

--- packages/design-system/providers/notifications.tsx
+++ packages/design-system/providers/notifications.tsx
@@ -1,15 +1,17 @@
+'use client';
+
 import { KnockFeedProvider, KnockProvider } from '@knocklabs/react';
 import type { ReactNode } from 'react';
 
-const knockApiKey = process.env.KNOCK_API_KEY;
-const knockFeedChannelId = process.env.KNOCK_FEED_CHANNEL_ID;
+const knockApiKey = process.env.NEXT_PUBLIC_KNOCK_API_KEY;
+const knockFeedChannelId = process.env.NEXT_PUBLIC_KNOCK_FEED_CHANNEL_ID;
 
 if (!knockApiKey) {
-  throw new Error('KNOCK_API_KEY is not set');
+  throw new Error('NEXT_PUBLIC_KNOCK_API_KEY is not set');
 }
 
 if (!knockFeedChannelId) {
-  throw new Error('KNOCK_FEED_CHANNEL_ID is not set');
+  throw new Error('NEXT_PUBLIC_KNOCK_FEED_CHANNEL_ID is not set');
 }
 
 export const NotificationsProvider = ({
