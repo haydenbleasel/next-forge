aa860f9a6875c7d65d5d7f67c5f87133524932e3
Hayden Bleasel <hello@haydenbleasel.com>
2025-01-14 19:41:34 -0500

    Move search package to addon

--- packages/search/index.ts
+++
@@ -1,8 +0,0 @@
-import Fuse from 'fuse.js';
-
-export const createSearch = <T>(data: readonly T[], keys: string[]): Fuse<T> =>
-  new Fuse(data, {
-    keys,
-    minMatchCharLength: 1,
-    threshold: 0.3,
-  });
