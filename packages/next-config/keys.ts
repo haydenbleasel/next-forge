7b7ac78b3360acffc1b9cc8ac971bac79bb4a275
Hayden Bleasel <hello@haydenbleasel.com>
2025-02-09 18:04:58 -0500

    For #367

--- packages/next-config/keys.ts
+++ packages/next-config/keys.ts
@@ -1,4 +1,4 @@
-import { vercel } from '@t3-oss/env-core/presets';
+import { vercel } from '@t3-oss/env-core/presets-zod';
 import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
