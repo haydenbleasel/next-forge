22fbd6902c208c08940afa598f4d097e81dac6a0
Hayden Bleasel <hello@haydenbleasel.com>
2025-03-05 15:06:31 -0500

    Fix logging

--- apps/app/next.config.ts
+++ apps/app/next.config.ts
@@ -1,10 +1,10 @@
 import { env } from '@/env';
 import { withToolbar } from '@repo/feature-flags/lib/toolbar';
 import { config, withAnalyzer } from '@repo/next-config';
-import { withLogtail, withSentry } from '@repo/observability/next-config';
+import { withLogging, withSentry } from '@repo/observability/next-config';
 import type { NextConfig } from 'next';
 
-let nextConfig: NextConfig = withToolbar(withLogtail({ ...config }));
+let nextConfig: NextConfig = withToolbar(withLogging(config));
 
 if (env.VERCEL) {
   nextConfig = withSentry(nextConfig);
