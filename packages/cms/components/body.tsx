d65521f1b4d9c65e3ea006102d0a93ddbd2e9945
Hayden Bleasel <hello@haydenbleasel.com>
2025-04-06 12:14:53 -0700

    Move fix into CMS package

--- packages/cms/components/body.tsx
+++ packages/cms/components/body.tsx
@@ -1,1 +1,21 @@
-export { RichText as Body } from 'basehub/react-rich-text';
+import { RichText, type RichTextProps } from 'basehub/react-rich-text';
+
+export const Body = ({
+  content,
+  components,
+}: {
+  content: RichTextProps['content'];
+  components: RichTextProps['components'];
+}) => {
+  return (
+    <RichText
+      content={content}
+      components={{
+        ul: ({ children, isTasksList, ...props }) => (
+          <ul {...props}>{children}</ul>
+        ),
+        ...components,
+      }}
+    />
+  );
+};
