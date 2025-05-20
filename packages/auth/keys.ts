4dad21f4ac847c42f2e0ff3e9097ac7dc89b8590
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-20 12:19:08 -0700

    Revert Clerk keyless change (not ready for prod)

--- packages/auth/keys.ts
+++ packages/auth/keys.ts
@@ -8,14 +8,11 @@ export const keys = () =>
       CLERK_WEBHOOK_SECRET: z.string().min(1).startsWith('whsec_').optional(),
     },
     client: {
-      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z
-        .string()
-        .startsWith('pk_')
-        .optional(),
-      NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().startsWith('/'),
-      NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().startsWith('/'),
-      NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().startsWith('/'),
-      NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string().startsWith('/'),
+      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1).startsWith('pk_'),
+      NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().min(1).startsWith('/'),
+      NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().min(1).startsWith('/'),
+      NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().min(1).startsWith('/'),
+      NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string().min(1).startsWith('/'),
     },
     runtimeEnv: {
       CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
