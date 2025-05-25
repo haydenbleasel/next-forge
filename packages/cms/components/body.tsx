4b5f0a112b56a644b6ef01c9eba0401dc35123f4
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-25 12:32:42 -0700

    Build fixes

--- packages/cms/components/body.tsx
+++ packages/cms/components/body.tsx
@@ -1,21 +1,3 @@
-import { RichText, type RichTextProps } from 'basehub/react-rich-text';
+import { RichText } from 'basehub/react-rich-text';
 
-export const Body = ({
-  content,
-  components,
-}: {
-  content?: RichTextProps['content'];
-  components?: RichTextProps['components'];
-}) => {
-  return (
-    <RichText
-      content={content}
-      components={{
-        ul: ({ children, isTasksList, ...props }) => (
-          <ul {...props}>{children}</ul>
-        ),
-        ...components,
-      }}
-    />
-  );
-};
+export const Body = RichText;
--- packages/payments/index.ts
+++ packages/payments/index.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 import { keys } from './keys';
 
 export const stripe = new Stripe(keys().STRIPE_SECRET_KEY, {
-  apiVersion: '2025-03-31.basil',
+  apiVersion: '2025-04-30.basil',
 });
 
 export type { Stripe } from 'stripe';
