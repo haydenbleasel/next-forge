6126907b57c9934306e8aeef8015418a0fa2b559
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-25 13:32:04 -0700

    Update layout.tsx

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,18 +1,37 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import { cn } from 'fumadocs-ui/utils/cn';
+import { Geist, Geist_Mono } from 'next/font/google';
 import type { ReactNode } from 'react';
 
-const inter = Inter({
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const sans = Geist({
   subsets: ['latin'],
+  variable: '--font-sans',
 });
 
-export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
-      </body>
-    </html>
-  );
-}
+const mono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+});
+
+const Layout = ({ children }: LayoutProps) => (
+  <html
+    lang="en"
+    className={cn(
+      'touch-manipulation font-sans antialiased',
+      sans.variable,
+      mono.variable
+    )}
+    suppressHydrationWarning
+  >
+    <body className="flex min-h-screen flex-col">
+      <RootProvider>{children}</RootProvider>
+    </body>
+  </html>
+);
+
+export default Layout;
