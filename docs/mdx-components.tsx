da55100a4a2d4c3f447114f1b567313aa96cfb8c
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-25 13:03:54 -0700

    Map Mintlify components to Fumadocs

--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -1,6 +1,7 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 
+import { Callout } from 'fumadocs-ui/components/callout';
 import { Authors } from './components/authors';
 import { VercelButton } from './components/vercel';
 
@@ -11,5 +12,8 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ...components,
     Authors,
     VercelButton,
+    Warning: ({ children }) => <Callout type="warning">{children}</Callout>,
+    Tip: ({ children }) => <Callout type="info">{children}</Callout>,
+    Info: ({ children }) => <Callout type="info">{children}</Callout>,
   };
 }
