021d4d39c3e2c3fc7c55e3a23199454b07fe473f
jpvalery <jpvalery@users.noreply.github.com>
2025-06-04 16:52:51 -0400

    fix: better dark/light support on unauthenticated

--- apps/app/app/(unauthenticated)/layout.tsx
+++ apps/app/app/(unauthenticated)/layout.tsx
@@ -9,15 +9,15 @@ type AuthLayoutProps = {
 const AuthLayout = ({ children }: AuthLayoutProps) => (
   <div className="container relative grid h-dvh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
     <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-      <div className="absolute inset-0 bg-zinc-900" />
-      <div className="relative z-20 flex items-center font-medium text-lg">
+      <div className="absolute inset-0 bg-muted" />
+      <div className="relative z-20 flex items-center font-medium text-lg text-primary">
         <CommandIcon className="mr-2 h-6 w-6" />
         Acme Inc
       </div>
       <div className="absolute top-4 right-4">
         <ModeToggle />
       </div>
-      <div className="relative z-20 mt-auto">
+      <div className="relative z-20 mt-auto text-primary">
         <blockquote className="space-y-2">
           <p className="text-lg">
             &ldquo;This library has saved me countless hours of work and helped
